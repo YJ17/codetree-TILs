180be9f309284ac6839e8802db36f583941d8d0a
YJ17 <pl3434on@gmail.com>
2024-04-05 04:00:16 +0900

    Title: 홀수만 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/홀수만 출력/output-only-odd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a;
+    int b;
+
+    scanf("%d %d", &a, &b);
+
+    for(a; a <= b; a+=2){
+        printf("%d ", a);
+    }
+
+    return 0;
+}
