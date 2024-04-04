b687f0c86f2ea7a3ba4f89764678cac66ac202ba
YJ17 <pl3434on@gmail.com>
2024-04-04 21:06:39 +0900

    Title: 합을 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240404/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 1;
+    int b = 2;
+    int c = 3;
+
+    a = a+b+c;
+    b = a;
+    c = a;
+
+    printf("%d %d %d", a, b, c);
+    return 0;
+}
