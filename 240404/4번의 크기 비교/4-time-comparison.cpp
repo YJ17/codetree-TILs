ca32bda0e5f17c1806a080134ee4b1f84dd29c32
YJ17 <pl3434on@gmail.com>
2024-04-04 22:47:14 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240404/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+
+    scanf("%d\n%d %d %d %d", &a, &b, &c, &d, &e);
+
+    printf("%d\n", a > b);
+    printf("%d\n", a > c);
+    printf("%d\n", a > d);
+    printf("%d\n", a > e);
+
+
+    return 0;
+}
