d7bb37b2a6b4e5f800df6635f2e708a92fd2ffb0
YJ17 <pl3434on@gmail.com>
2024-04-05 05:17:30 +0900

    Title: b부터 a까지 감소, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/b부터 a까지 감소/dec-from-b-to-a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a;
+    int b;
+
+    scanf("%d %d", &a, &b);
+
+    for(b; b >= a; b--){
+        printf("%d ", b);
+    }
+
+    return 0;
+}
