42cfd48fbe8b7031e3292b96c339e2781c4d832b
YJ17 <pl3434on@gmail.com>
2024-04-04 21:49:00 +0900

    Title: 합과 차의 나눗셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240404/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    int b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("%0.2f", (float)(a+b)/(a-b));
+    
+    return 0;
+}
