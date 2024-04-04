50f222c0567b93f91c741eb0f696667bf311c93e
YJ17 <pl3434on@gmail.com>
2024-04-05 05:22:43 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    int i = 1;
+
+    scanf("%d", &n);
+
+    while(i <= n){
+        printf("%d ", i);
+        i++;
+    }
+
+    return 0;
+}
