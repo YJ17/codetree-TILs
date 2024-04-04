865fe40e40d6e7a17712dcc9adf7ff0cd2d27a77
YJ17 <pl3434on@gmail.com>
2024-04-05 03:41:01 +0900

    Title: 중앙값 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240405/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a;
+    int b;
+    int c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a < b){
+        if(a < c){
+            if(b < c){
+                printf("%d", b);
+            }
+            else {
+                printf("%d", c);
+            }
+        }
+        else {
+            printf("%d", a);
+        }
+    }else if(a > c){
+        if(b < c){
+            printf("%d", b);
+        }
+        else {
+            printf("%d", c);
+        }
+    }else {
+        printf("%d", a);
+    }
+
+
+
+    return 0;
+}
