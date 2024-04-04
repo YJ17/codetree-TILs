93211c1089161cc7949fa13575ca254c5f2d7e52
YJ17 <pl3434on@gmail.com>
2024-04-04 22:39:56 +0900

    Title: 특정 조건 두 정수 비교, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240404/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a;
+    int b;
+
+    scanf("%d %d", &a, &b);
+
+    if(a < b){
+        printf("1 ");
+    }
+    else {
+        printf("0 ");
+    }
+
+    
+    if(a == b){
+        printf("1 ");
+    }
+    else {
+        printf("0 ");
+    }
+
+
+    return 0;
+}
