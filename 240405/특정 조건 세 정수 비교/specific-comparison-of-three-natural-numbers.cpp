860c3744cde2aca3cbff8a5daa2f714f8cbab4d4
YJ17 <pl3434on@gmail.com>
2024-04-05 02:23:25 +0900

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240405/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a;
+    int b;
+    int c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+
+    printf("%d %d",a < b && a < c, a==b==c);
+
+    return 0;
+}
