e81edd6923a869cf5850704597213975752816a8
YJ17 <pl3434on@gmail.com>
2024-04-04 21:37:25 +0900

    Title: 간단한 사칙연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240404/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a;
+    int b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("%d\n%d\n%d\n%d", a+b, a-b, a/b, a%b);
+
+    return 0;
+}
