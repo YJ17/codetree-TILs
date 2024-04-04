79277fb10a9a02c441c589c49b6530a2f976a000
YJ17 <pl3434on@gmail.com>
2024-04-04 21:10:21 +0900

    Title: 정수 세 개 입력받아 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240404/정수 세 개 입력받아 출력/take-three-integers-and-output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    int b;
+    int c;
+
+    scanf("%d %d\n%d", &a, &b, &c);
+
+    printf("%d %d %d", a, b, c);
+    return 0;
+}
