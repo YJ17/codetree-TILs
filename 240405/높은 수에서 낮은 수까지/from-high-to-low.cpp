9da57fb084f04d6d83e0a76f002a7597510e2361
YJ17 <pl3434on@gmail.com>
2024-04-05 06:36:00 +0900

    Title: 높은 수에서 낮은 수까지, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/높은 수에서 낮은 수까지/from-high-to-low.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a;
+    int b;
+
+    scanf("%d %d", &a, &b);
+
+    if(a > b){
+        for(a; a >= b; a--){
+            printf("%d ", a);
+        }
+    }
+    else {
+        for(b; b >= a; b--){
+            printf("%d ", b);
+        }
+    }
+
+    return 0;
+}
