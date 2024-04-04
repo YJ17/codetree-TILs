2a9ac9553e85835c4e87780f55a9b32effee491e
YJ17 <pl3434on@gmail.com>
2024-04-05 03:11:26 +0900

    Title: 코로나 메뉴얼, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240405/코로나 메뉴얼/covid-manual.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int p1;
+    int p2;
+    int p3;
+    char g1;
+    char g2;
+    char g3;
+
+    scanf("%c %d\n%c %d\n%c %d", &g1, &p1, &g2, &p2, &g3, &p3);
+
+    if(g1 == 'N'){
+        if(g2 == 'Y' && g3 == 'Y'){
+            if(p2 >= 37 && p3 >= 37){
+                printf("E");
+            }
+        }
+    }
+    else if(g2 == 'N'){
+        if(g1 == 'Y' && g3 == 'Y'){
+            if(p1 >= 37 && p3 >= 37){
+                printf("E");
+            }
+        }
+    }
+    else if(g3 == 'N'){
+        if(g1 == 'Y' && g2 == 'Y'){
+            if(p1 >= 37 && p2 >= 37){
+                printf("E");
+            }
+        }
+    }
+    else{
+        printf("N");
+    }
+
+
+
+    return 0;
+}
