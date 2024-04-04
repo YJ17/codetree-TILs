d0d14a3eae984e28d2669602afc82116407b161f
YJ17 <pl3434on@gmail.com>
2024-04-05 02:26:03 +0900

    Title: 세 정수의 최솟값, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a;
+    int b;
+    int c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a <= b && a <= c){
+        printf("%d", a);
+    }
+    else if(b <= a && b <= c){
+        printf("%d", b);
+    }
+    else {
+        printf("%d", c);
+    }
+
+
+    return 0;
+}
