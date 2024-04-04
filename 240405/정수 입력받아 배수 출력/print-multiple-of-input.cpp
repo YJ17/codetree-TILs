3ffc64d987358c57f64dbd508b858d1187dbea8d
YJ17 <pl3434on@gmail.com>
2024-04-05 03:58:18 +0900

    Title: 정수 입력받아 배수 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+
+    scanf("%d", &n);
+
+    for(int i = 1; i <= 5; i++){
+        printf("%d ", n*i);
+    }
+
+    return 0;
+}
