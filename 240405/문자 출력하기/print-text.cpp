05ffcd295376007c652309903ee9f62c473040f5
YJ17 <pl3434on@gmail.com>
2024-04-05 05:30:47 +0900

    Title: 문자 출력하기, Time: 16ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/문자 출력하기/print-text.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    char c;
+
+    scanf("%c", &c);
+
+    for(int i = 0; i < 8; i++){
+        printf("%c", c);
+    }
+
+    return 0;
+}
