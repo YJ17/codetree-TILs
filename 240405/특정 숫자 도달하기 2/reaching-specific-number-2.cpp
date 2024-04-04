f8a009096ded7b298cd7080c8cbad372801227d5
YJ17 <pl3434on@gmail.com>
2024-04-05 05:15:41 +0900

    Title: 특정 숫자 도달하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/특정 숫자 도달하기 2/reaching-specific-number-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+
+    scanf("%d", &n);
+
+    for(n; n >= 1; n--){
+        printf("%d ", n);        
+    }
+
+    return 0;
+}
