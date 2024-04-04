176db629b5dc47f40ab39f181d4a043918ee0cf5
YJ17 <pl3434on@gmail.com>
2024-04-04 21:56:05 +0900

    Title: 음수 구별하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240404/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+
+    scanf("%d", &n);
+
+    printf("%d", n);
+
+    if(n < 0){
+        printf("\nminus");
+    }
+
+    return 0;
+}
