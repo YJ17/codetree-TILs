8fe20fa6f358b5177ab2023a046aa632aaf8b44f
YJ17 <pl3434on@gmail.com>
2024-04-06 03:54:18 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240406/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    bool s = false;
+
+    scanf("%d", &n);
+
+    for(int i = 2; i < n; i++){
+        if(n % i == 0){
+            s = true;
+        }
+    }
+
+    if(s == true){
+        printf("C");
+    }
+    else{
+        printf("N");
+    }
+
+    return 0;
+}
