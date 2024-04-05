fb274587c15f2c2d22755aff39234489c9531781
YJ17 <pl3434on@gmail.com>
2024-04-06 07:42:14 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240406/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    int c = 1;
+
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(i <= j){
+                printf("%d ", c);
+                c++;
+                if(c == 10){
+                    c = 1;
+                }
+            }
+            else{
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
