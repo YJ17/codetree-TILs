28988c32b677d2327e4847bf01b5e71f1f42bec2
YJ17 <pl3434on@gmail.com>
2024-04-05 22:29:55 +0900

    Title: 배수의 개수 2, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/배수의 개수 2/number-of-multipliers-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    int c = 0;
+
+    for(int i = 0 ; i <= 9; i++){
+            scanf("%d", &n);
+            if(n % 2 != 0){
+                c++;
+            }
+    }
+
+    printf("%d", c);
+    
+
+
+    return 0;
+}
