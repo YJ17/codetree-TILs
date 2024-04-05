3f3375d99e0d57a8760e8e5d41a60e1d47480f21
YJ17 <pl3434on@gmail.com>
2024-04-06 04:24:17 +0900

    Title: 별표 출력하기 5, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240406/별표 출력하기 5/print-star-5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+
+    scanf("%d", &n);
+
+    for(int i = n; i >= 1; i--){
+        for(int j = i; j >= 1; j--){
+            for(int k = i; k >= 1; k--){
+                printf("*");
+            }
+            printf(" ");
+        }
+        printf("\n");
+    }
+
+
+
+    return 0;
+}
