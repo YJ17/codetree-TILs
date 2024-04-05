f20141ce9df53bf47f261f816b03703cc0ef8119
YJ17 <pl3434on@gmail.com>
2024-04-06 08:42:13 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240406/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int arr[10];
+    int s = 0;
+
+    for(int i = 0; i < 10; i++){
+        scanf("%d ", &arr[i]);
+        s += arr[i];
+    }
+
+    printf("%d", s);
+
+    return 0;
+}
