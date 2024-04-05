296be9b9bfe1f6d2732f4bce0184edde4bba746f
YJ17 <pl3434on@gmail.com>
2024-04-05 23:09:09 +0900

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/a의 b승/a-to-the-power-of-b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a;
+    int b;
+    int t = 1;
+
+    scanf("%d %d", &a, &b);
+
+    for(int i = 1; i <= b; i++){
+        t *= a;
+    }
+
+    printf("%d", t);
+
+    return 0;
+}
