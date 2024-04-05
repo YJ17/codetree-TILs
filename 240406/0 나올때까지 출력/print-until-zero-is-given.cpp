14df3167be12a0cae38e450971d54e40f484c26e
YJ17 <pl3434on@gmail.com>
2024-04-06 03:04:48 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240406/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+
+    while(1){
+        scanf("%d", &n);
+        if(n == 0){
+            break;
+        }
+        printf("%d\n", n);
+    }
+
+    return 0;
+}
