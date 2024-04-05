c3c9fd59aebb1812729660659206aafc83dfee94
YJ17 <pl3434on@gmail.com>
2024-04-06 01:37:19 +0900

    Title: a의 배수들의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240406/a의 배수들의 곱/product-of-multiples-of-a.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a;
+    int b;
+    int t = 1;
+
+    scanf("%d %d", &a, &b);
+
+    for(int i = a; i <= b; i++){
+        if(i % a == 0){
+            t *= i;
+        }
+    }
+
+    printf("%d", t);
+
+    return 0;
+}
