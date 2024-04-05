0d55ee0abf3bd8011dd5f59c291209deebd30bd6
YJ17 <pl3434on@gmail.com>
2024-04-05 22:55:10 +0900

    Title: 5의 배수의 합, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/5의 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a;
+    int b;
+    int sum = 0;
+
+    scanf("%d %d", &a, &b);
+
+    if(a < b){
+        for(a; a <= b; a++){
+            if(a % 5 == 0){
+                sum += a;
+            }
+        }
+    }
+    else {
+        for(b; b <= a; b++){
+            if(b % 5 == 0){
+                sum += b;
+            }
+        }
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
