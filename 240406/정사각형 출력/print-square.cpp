4fe9cd667cac65d32ce730171dc0db610f5d6170
YJ17 <pl3434on@gmail.com>
2024-04-06 06:55:39 +0900

    Title: 정사각형 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240406/정사각형 출력/print-square.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    int c = 1;
+
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            printf("%d ", c);
+            c++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
