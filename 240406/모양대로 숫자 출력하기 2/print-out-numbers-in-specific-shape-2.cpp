db7cdd996af4fb902b21f042bb0653881b4b8005
YJ17 <pl3434on@gmail.com>
2024-04-06 06:59:36 +0900

    Title: 모양대로 숫자 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240406/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    int c = 1;
+
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            printf("%d ", c * 2);
+            c++;
+            if(c == 5){
+                c = 1;
+            }
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
