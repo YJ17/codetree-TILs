f2f063f84c7c767ad1c0d5de2206e23c1e7b86a8
YJ17 <pl3434on@gmail.com>
2024-04-06 03:57:00 +0900

    Title: 공약수의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240406/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
+++ 240406/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
@@ -10,7 +10,7 @@ int main() {
     scanf("%d %d", &a, &b);
 
     for(int i = a; i <= b; i++){
-        if(1920 % i == 0 || 2880 % i == 0){
+        if(1920 % i == 0 && 2880 % i == 0){
             s = true;
         }
     }
