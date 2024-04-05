a087a925ec96b85f2c3e475469a8333825cf17db
YJ17 <pl3434on@gmail.com>
2024-04-06 06:36:15 +0900

    Title: 규칙을 찾아 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240406/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
+++ 240406/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
@@ -9,7 +9,7 @@ int main() {
 
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= n; j++){
-            if(i == 1 || i == 6 || j == 1 || j == 6){
+            if(i == 1 || i == n || j == 1 || j == n){
                 printf("* ");
             }
             else if(i > j){
