5a5b95c74810f464e0718ec1575397854092e526
YJ17 <pl3434on@gmail.com>
2024-04-06 03:10:02 +0900

    Title: 출석 부르기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240406/출석 부르기 2/calling-attendance-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+
+    while(1){
+        scanf("%d", &n);
+        if(n == 1){
+            printf("John\n");
+        }
+        else if(n == 2){
+            printf("Tom\n");
+        }
+        else if(n == 3){
+            printf("Paul\n");
+        }
+        else if(n == 4){
+            printf("Sam\n");
+        }
+        else {
+            printf("Vacancy\n");
+            break;
+        }
+    }
+
+    return 0;
+}
