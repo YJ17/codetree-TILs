17f7de8fd1e595a630a2cd8796f7e4b43dc53aa4
YJ17 <pl3434on@gmail.com>
2024-04-06 07:30:32 +0900

    Title: 구구단 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240406/구구단 만들기 2/print-multiplication-table-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a;
+    int b;
+
+    scanf("%d %d", &a, &b);
+
+    for(int i = 2; i <= 8; i+=2){
+        for(int j = b; j >= a; j--){
+            printf("%d * %d = %d", j, i, j*i);
+            if(j > a){
+                printf(" / ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
