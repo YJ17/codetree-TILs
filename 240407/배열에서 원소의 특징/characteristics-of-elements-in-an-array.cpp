448c40768c1257b7018eed62454d2eeda8aa3c8e
YJ17 <pl3434on@gmail.com>
2024-04-07 08:55:53 +0900

    Title: 배열에서 원소의 특징, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240407/배열에서 원소의 특징/characteristics-of-elements-in-an-array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int arr[11];
+
+    for(int i = 0; i < 10; i++){
+        scanf("%d", &arr[i]);
+        if(arr[i] % 3 == 0){
+            printf("%d", arr[i-1]);
+            break;
+        }
+    }
+
+    return 0;
+}
