50773f02059a7fab27e071774bb77fbdbb6870bb
YJ17 <pl3434on@gmail.com>
2024-04-07 07:59:59 +0900

    Title: 10개 입력받기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240407/10개 입력받기/receive-10-inputs.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    int sum = 0;
+    int c = 0;
+
+    for(int i = 0; i < 9; i++){
+        scanf("%d", &n);
+        if(n != 0){
+            sum += n;
+            c++;
+        }
+        else{
+            break;
+        }
+    }
+
+    printf("%d %0.1f", sum, (float)sum/c);
+
+    return 0;
+}
