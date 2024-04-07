4d966c0347a993b52179ff20b6231fe7626ab0f6
YJ17 <pl3434on@gmail.com>
2024-04-07 22:41:01 +0900

    Title: 나눗셈의 나머지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240407/나눗셈의 나머지/remainder-of-division.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a;
+    int b;
+    int arr[10] = {};
+    int s = 0;
+
+    scanf("%d %d", &a, &b);
+
+    while(a >= 1){
+        arr[a % b] = a % b;
+        a /= b;
+    }
+
+    for(int i = 0; i < 10; i++){
+        if(arr[i] != 0){
+            s = arr[i] * arr[i];
+        }
+    }
+
+    printf("%d", s);
+
+    return 0;
+}
