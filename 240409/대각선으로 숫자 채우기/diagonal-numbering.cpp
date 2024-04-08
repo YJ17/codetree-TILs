b95d3ad40fa065e0d3b8afe8b4896debdb322a4f
YJ17 <pl3434on@gmail.com>
2024-04-09 02:36:29 +0900

    Title: 대각선으로 숫자 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240409/대각선으로 숫자 채우기/diagonal-numbering.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    int m;
+    int arr[101][101] = {};
+    int num = 1;
+    int c = 0;
+    int i = 0;
+    int j = 0;
+
+    scanf("%d %d", &n, &m);
+
+    while(1){
+        arr[i][j] = num;
+        num++;
+        printf("arr[%d][%d] = %d   c = %d\n", i, j, num-1, c);
+        if(i == n-1 && j == m-1){
+            break;
+        }
+
+        if(j == 0 && i < n-1 && c == 0){
+            if(i + 1 == m-1){
+                c += 1;
+            }
+            j = i+1;
+            i = 0;
+        }
+        else if(j == 0 && i < n-1 && c != 0){
+               j = m-1;
+            if(c >= n){
+                c = n-1;
+            }
+            i = c;
+            c += 1;
+        } else if (i == n-1){
+            j = m-1;
+            if(c >= n){
+                c = n-1;
+            }
+            i = c;
+            c += 1;
+        }
+        else {
+            j -= 1;
+            i += 1;
+        }
+    }
+
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+
+
+
+
+    return 0;
+}
