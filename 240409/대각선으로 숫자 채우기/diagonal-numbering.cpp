ede6b112a708d539f45d0ea0b9b1c42ead7009fb
YJ17 <pl3434on@gmail.com>
2024-04-09 03:13:36 +0900

    Title: 대각선으로 숫자 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240409/대각선으로 숫자 채우기/diagonal-numbering.cpp
+++ 240409/대각선으로 숫자 채우기/diagonal-numbering.cpp
@@ -7,68 +7,54 @@ int main() {
     int m;
     int arr[101][101] = {};
     int num = 1;
-    int c = 0;
+    int ci = 0;
+    int cj = 0;
     int i = 0;
     int j = 0;
 
     scanf("%d %d", &n, &m);
 
-    while(n != 1 && m != 1){
+    while(1){
         arr[i][j] = num;
         num++;
         if(i == n-1 && j == m-1){
             break;
         }
 
-        if(j == 0 && i < n-1 && c == 0){
-            if(i + 1 == m-1){
-                c += 1;
-            }
-            j = i+1;
+        if(j == 0 && i + 1 >= m){
+            ci += 1;
+            j = m-1;
+            i = ci;
+        }
+        else if(j == 0){
+            cj += 1;
+            j = cj;
             i = 0;
         }
-        else if(j == 0 && i < n-1 && c != 0){
+        else if(i == n-1 && cj + 1 == m){
+            ci += 1;
             j = m-1;
-            if(c >= n){
-                c = n-1;
-            }
-            i = c;
-            c += 1;
-        } else if (i == n-1){
-            j = m-1;
-            if(c >= n){
-                c = n-1;
-            }
-            i = c;
-            c += 1;
+            i = ci;
+        }
+        else if(i == n-1){
+            cj += 1;
+            j = cj;
+            i = 0;
         }
         else {
             j -= 1;
             i += 1;
         }
+
+
     }
 
-    if(n == 1){
-        for(int i = 1; i <= m; i++){
-            printf("%d ", i);
-        }
-    }
-    else if(m == 1){
-        for(int i = 1; i <= n; i++){
-            printf("%d\n", i);
-        }
-    }
-    else{
-        for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 printf("%d ", arr[i][j]);
             }
             printf("\n");
         }
-    }
-
-
-
 
 
     return 0;
