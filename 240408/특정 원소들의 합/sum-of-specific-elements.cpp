2772fc7fdaed42f6614af96bce0003152d35c13e
YJ17 <pl3434on@gmail.com>
2024-04-08 23:10:19 +0900

    Title: 특정 원소들의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240408/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int arr[4][4];
+    int sum = 0;
+
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            scanf("%d ", &arr[i][j]);
+        }
+    }
+
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            if(i >= j){
+                sum += arr[i][j];
+            }
+        }
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
