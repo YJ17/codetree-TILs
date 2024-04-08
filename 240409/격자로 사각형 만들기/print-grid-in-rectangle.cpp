3a5d9ebd82f3dc6ec71f32c9595e440f6a23b7a5
YJ17 <pl3434on@gmail.com>
2024-04-09 07:53:54 +0900

    Title: 격자로 사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240409/격자로 사각형 만들기/print-grid-in-rectangle.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    int arr[10][10];
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++){
+        arr[i][0] = 1;
+        arr[0][i] = 1;
+    }
+
+    for(int i = 1; i < n; i++){
+        for(int j = 1; j < n; j++){
+            arr[i][j] = arr[i - 1][j] + arr[i][j - 1] + arr[i - 1][j - 1];
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
