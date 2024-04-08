e0733cc470166405ac8870acc0ac293b7a7bd9ae
YJ17 <pl3434on@gmail.com>
2024-04-08 23:36:10 +0900

    Title: 지그재그로 숫자 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240408/지그재그로 숫자 채우기/zigzag-numbering.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    int m;
+    int num = 0;
+    int arr[101][101];
+
+    scanf("%d %d", &n, &m);
+
+    for(int j = 0 ; j < m; j++){
+
+        if(j % 2 == 0){
+            for(int i = 0 ; i < n; i++){
+                arr[i][j] = num;
+                num++;
+            }
+        }
+        else {
+            for(int i = n-1; i >= 0; i--){
+                arr[i][j] = num;
+                num++;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0 ; j < m; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
