e4a998822d5ea7e878e71828bb65edcbc6d7a3aa
YJ17 <pl3434on@gmail.com>
2024-04-08 19:11:39 +0900

    Title: 특정 위치의 문자, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240408/특정 위치의 문자/char-in-specific-location.cpp
+++ 240408/특정 위치의 문자/char-in-specific-location.cpp
@@ -5,13 +5,19 @@ int main() {
 
     char arr[6] = {'L', 'E', 'B', 'R', 'O', 'S'};
     char c;
+    int count = 0;
 
     scanf("%c", &c);
 
     for(int i = 0; i < 6; i++){
         if(arr[i] == c){
             printf("%d ", i);
+            count++;
         }
+    }
+
+    if(count = 0){
+        printf("None");
     }
 
     return 0;
