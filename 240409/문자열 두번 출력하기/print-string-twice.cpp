d03590f2fbf575a0fd76a72814621c03581970e2
YJ17 <pl3434on@gmail.com>
2024-04-09 08:11:27 +0900

    Title: 문자열 두번 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240409/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    char arr[100];
+
+    scanf("%s", arr);
+
+    for(int i = 0; i < 2; i++){
+        printf("%s\n", arr);
+    }
+
+    return 0;
+}
