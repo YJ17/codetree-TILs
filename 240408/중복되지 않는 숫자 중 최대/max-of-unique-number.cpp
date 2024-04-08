5823db3e25df9b03b29e3f70af72c2bd91a5511e
YJ17 <pl3434on@gmail.com>
2024-04-08 22:10:51 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240408/중복되지 않는 숫자 중 최대/max-of-unique-number.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    int arr[1001];
+    int max = -1;
+    int bb[1001] = {};
+    bool t = false;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = i+1; j < n; j++){
+            if(arr[i] == arr[j]){
+                bb[i] = 1;
+                bb[j] = 1;
+            }
+            else if(bb[j] != 1) {
+                bb[j] = 2;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        if(bb[i] == 2 && max < arr[i]){
+            max = arr[i];
+            t = true;
+        }
+    }
+
+    if(t == true){
+        printf("%d", max);
+    }
+    else {
+        printf("-1");
+    }
+
+    return 0;
+}
