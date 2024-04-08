3f0683c67596de65fddba25858db61440246eb35
YJ17 <pl3434on@gmail.com>
2024-04-09 08:28:15 +0900

    Title: 문자열 범위 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240409/문자열 범위 출력하기/print-string-in-range.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    char a[101];
+
+    // fgets(a, 101, stdin);
+    scanf("%[^\n]", a);
+
+    for(int i = 2; i < 10; i++){
+        printf("%c", a[i]);
+    }
+
+
+    return 0;
+}
