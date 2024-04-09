6c35b6194044a0eb9fe93efb7cd2d49c2cc6b3e4
YJ17 <pl3434on@gmail.com>
2024-04-09 09:00:12 +0900

    Title: 단어로 구분하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240409/단어로 구분하기 2/separate-words-with-words-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    char arr[10][50];
+
+    for(int i = 0; i < 10; i++){
+        scanf("%s", arr[i]);
+        if(i % 2 == 0){
+            printf("%s\n", arr[i]);
+        }
+    }
+
+    return 0;
+}
