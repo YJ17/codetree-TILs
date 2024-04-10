b182d91b362b1b0d5c27769457ced52f53c0516c
YJ17 <pl3434on@gmail.com>
2024-04-10 18:31:24 +0900

    Title: 문자열 놀이, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240410/문자열 놀이/play-with-string.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    char s[101];
+    char temp;
+    int q;
+    int t;
+    int a;
+    int b;
+    char a1;
+    char b1;
+
+    scanf("%s ", s);
+    scanf("%d", &q);
+
+    for(int i = 0; i < q; i++){
+        scanf("%d ", &t);
+        if(t == 1){
+            scanf("%d %d", &a, &b);
+            for(int j = 0; j < strlen(s); j++){
+                temp = s[b - 1];
+                s[b - 1] = s[a - 1];
+                s[a - 1] = temp;
+            }
+            printf("%s\n", s);
+        }
+        else {
+            scanf("%c %c", &a1, &b1);
+            for(int j = 0; j < strlen(s); j++){
+                if(s[j] == a1){
+                    s[j] = b1;
+                }
+            }
+            printf("%s\n", s);
+        }
+    }
+
+    return 0;
+}
