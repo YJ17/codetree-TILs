169f4fc137bed8130f929bd655eed8652bd4cf04
YJ17 <pl3434on@gmail.com>
2024-04-10 15:49:24 +0900

    Title: 문자열 추가하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240410/문자열 추가하기/add-spring.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    char arr[30];
+
+    scanf("%s", arr);
+    strcat(arr, "Hello");
+    printf("%s", arr);
+
+    return 0;
+}
