6739c8b4b96ee1ebc557e26cb7b8ac90b89c26ae
YJ17 <pl3434on@gmail.com>
2024-04-10 15:29:44 +0900

    Title: Run Length 인코딩, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240410/Run Length 인코딩/run-length-encoding.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    char arr[1001];
+    int count[1001];
+    char alph[1001];
+    int c = 1;
+    int a_i = 0;
+    int c_i = 0;
+
+    scanf("%s", &arr);
+
+    for(int i = 0; i < strlen(arr); i++){
+        if(arr[i] == arr[i+1]){
+            c++;
+        }
+        else{
+            alph[a_i] = arr[i];
+            count[c_i] = c;
+            c = 1;
+            a_i++;
+            c_i++;
+        }
+    }
+
+    printf("%d\n", strlen(alph) * 2);
+    for(int i = 0; i < c_i; i++){
+        printf("%c", alph[i]);
+        printf("%d", count[i]);
+    }
+
+    return 0;
+}
