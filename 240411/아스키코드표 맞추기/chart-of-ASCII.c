712a9bccef04ad3353cec09e6b50876f8e6d6319
YJ17 <pl3434on@gmail.com>
2024-04-11 21:20:12 +0900

    Title: 아스키코드표 맞추기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240411/아스키코드표 맞추기/chart-of-ASCII.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+
+    for(int i = 0; i < 5; i++){
+        scanf("%d ", n);
+        printf("%c", (char)n);
+    }
+
+    return 0;
+}
