94957a55ce275bcfc6936abecbe14add9a0f2c35
YJ17 <pl3434on@gmail.com>
2024-04-12 01:37:39 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240412/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void PrintLineNtime(int n){
+    for(int i = 0; i < n; i++){
+        printf("12345^&*()_\n");
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+
+    scanf("%d", &n);
+
+    PrintLineNtime(n);
+
+    return 0;
+}
