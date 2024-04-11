9ea788b8538095ba5dda2ff111a7c02dcc7fc2fd
YJ17 <pl3434on@gmail.com>
2024-04-12 04:56:43 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240412/함수를 이용한 소수 판별/decimal-decisions-using-functions.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool IsSosu(int n){
+    for(int i = 2; i < n; i++){
+        if(n % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a, b;
+    int sum = 0;
+
+    scanf("%d %d", &a, &b);
+
+    for(int i = a; i <= b; i++){
+        if(IsSosu(i)){
+            sum += i;
+        }
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
