b139640087df06eaaa16e103ab8b5e28558ec2e3
YJ17 <pl3434on@gmail.com>
2024-04-12 07:13:55 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240412/그 계절, 그 날/that-season-that-day.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool IsRightDate(bool st, int m, int d) {
+    if (m > 12 || d > 31) {
+        return false;
+    }
+    else if (st && m == 2 && d > 28) {
+        return false;
+    }
+    else if ((m == 2 || m == 4 || m == 6 || m == 9 || m == 11) && d > 30) {
+        return false;
+    }
+    return true;
+}
+
+bool IsYun(int y) {
+    if (y % 4 == 0 && y % 100 == 0 && y % 400 == 0) {
+        return true;
+    }
+    else if (y % 4 == 0 && y % 100 == 0) {
+        return false;
+    }
+
+    return false;
+}
+
+void WhatWeather(int m) {
+    if (m >= 9 && m <= 11) {
+        printf("Fall");
+    }
+    else if (m >= 6) {
+        printf("Summer");
+    }
+    else if (m >= 3) {
+        printf("Spring");
+    }
+    else {
+        printf("Winter");
+    }
+}
+
+void IfIsExistDate(int y, int m, int d) {
+    if (IsRightDate(IsYun(y), m, d)) {
+        WhatWeather(m);
+    }
+    else {
+        printf("-1");
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int y, m, d;
+
+    scanf("%d %d %d", &y, &m, &d);
+
+    IfIsExistDate(y, m, d);
+
+    return 0;
+}
