a45c1edc9330f761b4794a7ee996d215c1fd9f84
YJ17 <pl3434on@gmail.com>
2024-04-11 20:59:03 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 15ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240411/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    char arr[1001];
+    char rev[1001];
+    char temp;
+    int qn;
+    int q;
+
+    scanf("%s ", arr);
+    scanf("%d", &qn);
+
+    for(int i = 0; i < qn; i++){
+        scanf("%d", &q);
+        if(q == 1){
+            temp = arr[0];
+            for(int i = 0; i < strlen(arr); i++){
+                arr[i] = arr[i + 1];
+            }
+            arr[strlen(arr)] = temp;
+            printf("%s\n", arr);
+        }
+        else if(q == 2){
+            temp = arr[strlen(arr) - 1];
+            for(int i = strlen(arr) - 1; i >= 1; i--){
+                arr[i] = arr[i - 1];
+            }
+            arr[0] = temp;
+            printf("%s\n", arr);
+        }
+        else {
+            for(int i = strlen(arr) - 1; i >= 0; i--){
+                rev[(strlen(arr) - 1) - i] = arr[i];
+                
+            }
+            strcpy(arr, rev);
+            printf("%s\n", arr);
+        }
+    }
+
+    return 0;
+}
