654ec31d3fa636556198ab032efae3fa454550b2
YJ17 <pl3434on@gmail.com>
2024-04-14 06:18:20 +0900

    Title: 문자열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240414/문자열의 개수/number-of-spring.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    string t[200];
+    int count = 0;
+
+    cin >> t[count];
+    count++;
+
+    while(t[count - 1] != "0"){
+        cin >> t[count];
+        count++;
+    }
+
+    cout << count - 1 << endl;
+
+    for(int i = 0; i < count; i++){
+        if(i % 2 == 0){
+            cout << t[i] << endl;
+        }
+    }
+
+
+    return 0;
+}
