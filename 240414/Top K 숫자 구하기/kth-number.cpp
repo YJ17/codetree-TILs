43cd486b09ca6ac85980930ad876cee3c755f1e8
YJ17 <pl3434on@gmail.com>
2024-04-14 22:22:46 +0900

    Title: Top K 숫자 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240414/Top K 숫자 구하기/kth-number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n, k;
+    int arr[1001];
+
+    cin >> n >> k;
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    cout << arr[k - 1];
+
+    return 0;
+}
