284f56dd370fa697a6742498de89c0e260ec9bbd
YJ17 <pl3434on@gmail.com>
2024-04-14 22:21:11 +0900

    Title: 단어 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240414/단어 정렬/sorting-words.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;    
+    string arr[101];
+
+    cin >> n;
+
+    for(int i =0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+      for(int i =0; i < n; i++){
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
