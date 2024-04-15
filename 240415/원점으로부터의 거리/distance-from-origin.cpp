02daa03f39d4ac6d38388679e2dd685221c06f51
YJ17 <pl3434on@gmail.com>
2024-04-15 22:55:29 +0900

    Title: 원점으로부터의 거리, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240415/원점으로부터의 거리/distance-from-origin.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Dot {
+    public :
+        int x, y, number;
+
+        Dot(int x, int y, int number){
+            this->x = x;
+            this->y = y;
+            this->number = number;
+        }
+
+        Dot() {}
+};
+
+bool cmp(const Dot &a, const Dot &b){
+    return a.x + a.y < b.x + b.y;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    Dot dots[1001];
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> dots[i].x >> dots[i].y;
+        dots[i].number = i + 1;
+    }
+
+    sort(dots, dots + n, cmp);
+
+    for(int i = 0; i < n; i++){
+        cout << dots[i].number << endl;
+    }
+
+    return 0;
+}
