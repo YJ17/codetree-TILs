cf6fe654fdd56fe6dc4a128511fdb46f7972b4e5
YJ17 <pl3434on@gmail.com>
2024-04-15 22:26:12 +0900

    Title: 개인정보, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240415/개인정보/personal-info.cpp
+++ 240415/개인정보/personal-info.cpp
@@ -30,6 +30,8 @@ int main() {
     // 여기에 코드를 작성해주세요.
 
     Student students[5];
+    cout << fixed;
+    cout.precision(1);
 
     for(int i = 0 ; i < 5; i++){
         cin >> students[i].name >> students[i].height >> students[i].weight;
