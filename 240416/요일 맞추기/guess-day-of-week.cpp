651c5e8b0713b1507c89713d05f36c71ffc3add0
YJ17 <pl3434on@gmail.com>
2024-04-16 07:02:52 +0900

    Title: 요일 맞추기, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240416/요일 맞추기/guess-day-of-week.cpp
+++ 240416/요일 맞추기/guess-day-of-week.cpp
@@ -8,7 +8,6 @@ int main() {
     int m1, m2, d1, d2, cal = 0;
     int month_date[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     string day[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-    string minusDay[7] = {"Mon", "Sun", "Sat", "Fri", "Thu", "Wed", "Tue"};
 
     cin >> m1 >> d1 >> m2 >> d2;
 
@@ -22,13 +21,11 @@ int main() {
     }
     cal -= d1;
 
-    if(cal < 0){
-        cout << minusDay[(-cal) % 7];
+    while(cal < 0){
+        cal += 7;
     }
-    else {
-        cout << day[cal % 7];
 
-    }
+    cout << day[cal % 7];
 
 
     return 0;
