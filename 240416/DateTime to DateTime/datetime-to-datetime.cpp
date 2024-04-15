3e52c0add6352c3610d62b221b6f6b3a4724b9ff
YJ17 <pl3434on@gmail.com>
2024-04-16 06:26:01 +0900

    Title: DateTime to DateTime, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240416/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    if(a < 11 || b < 11 || c < 11){
+        cout << -1;
+    }
+    else if(a - 11){
+        cout << (((a-11)*24)*60) + (b * 60 + c) - (11 * 60 + 11);
+    }
+    else {
+        cout << (b * 60 + c) - (11 * 60 + 11);
+    }
+
+    return 0;
+}
