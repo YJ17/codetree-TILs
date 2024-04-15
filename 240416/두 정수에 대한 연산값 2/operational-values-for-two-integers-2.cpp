c1661ac1ae3e283c0b0ac38cfc9825666bd199ba
YJ17 <pl3434on@gmail.com>
2024-04-16 05:40:07 +0900

    Title: 두 정수에 대한 연산값 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240416/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void DoMath(int &a, int &b){
+    if(a > b){
+        a *= 2;
+        b += 10;
+    }
+    else {
+        a += 10;
+        b *= 2;
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a, b;
+
+    cin >> a >> b;
+
+    DoMath(a, b);
+
+    cout << a << " " << b;
+
+    return 0;
+}
