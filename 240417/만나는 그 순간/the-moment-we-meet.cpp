55a446683183e9caf32d383621b35fe63af216f1
YJ17 <pl3434on@gmail.com>
2024-04-17 01:52:01 +0900

    Title: 만나는 그 순간, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240417/만나는 그 순간/the-moment-we-meet.cpp
+++ 240417/만나는 그 순간/the-moment-we-meet.cpp
@@ -10,7 +10,12 @@ int main() {
     char d;
     bool connect = false;
 
+    for(int i = 0; i < 1000; i++){
+        a[i] = b[i] = -100000;
+    }
+
     cin >> n >> m;
+
 
     for(int i = 0; i < n; i++){
         cin >> d >> t;
@@ -74,9 +79,12 @@ int main() {
     }
 
     for(int i = 1; i < 1000; i++){
-        if(a[i] == b[i]){
+        if(a[i] != -100000 && b[i] != -100000 && a[i] == b[i]){
             cout << i;
             connect = true;
+            break;
+        }
+        else if(a[i] == -100000 && b[i] == -100000){
             break;
         }
     }
