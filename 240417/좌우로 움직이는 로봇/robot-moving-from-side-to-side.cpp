09796f854eaa844388d5f01c6e2323e53e4295dc
YJ17 <pl3434on@gmail.com>
2024-04-17 07:38:56 +0900

    Title: 좌우로 움직이는 로봇, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240417/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+#define MAX_LEN 1000000
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n, m, t, count = 0;
+    int a_time = 0, b_time = 0;
+    int a_mov[MAX_LEN + 1], b_mov[MAX_LEN + 1];
+    char d;
+
+    a_mov[a_time] = 0;
+    b_mov[b_time] = 0;
+    a_time++;
+    b_time++;
+
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++){
+        cin >> t >> d;
+
+        for(int j = 0; j < t; j++){
+            if(d == 'R'){
+                a_mov[a_time] = a_mov[a_time - 1] + 1;
+            }
+            else {
+                a_mov[a_time] = a_mov[a_time - 1] - 1;
+            }
+            a_time++;
+        }
+    }
+
+
+    for(int i = 0; i < m; i++){
+        cin >> t >> d;
+
+        for(int j = 0; j < t; j++){
+            if(d == 'R'){
+                b_mov[b_time] = b_mov[b_time - 1] + 1;
+            }
+            else {
+                b_mov[b_time] = b_mov[b_time - 1] - 1;
+            }
+            b_time++;
+        }
+    }
+
+    if(a_time > b_time){
+        for(int i = 0; i < a_time - b_time; i++){
+            b_mov[b_time] = b_mov[b_time - 1];
+            b_time++;
+        }
+    }
+    else{
+        for(int i = 0; i < b_time - a_time; i++){
+            a_mov[a_time] = a_mov[a_time - 1];
+            a_time++;
+        }
+    }
+
+    for(int i = 1; i < a_time; i++){
+        if(a_mov[i - 1] != b_mov[i - 1] && a_mov[i] == b_mov[i]){
+            count++;
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
