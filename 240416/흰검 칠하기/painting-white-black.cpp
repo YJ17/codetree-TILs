87494b5da714fbbf8b0075eac49bbf3061d97818
YJ17 <pl3434on@gmail.com>
2024-04-16 09:29:56 +0900

    Title: 흰검 칠하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240416/흰검 칠하기/painting-white-black.cpp
+++ 240416/흰검 칠하기/painting-white-black.cpp
@@ -18,43 +18,45 @@ int main() {
     // 여기에 코드를 작성해주세요.
 
 
-    int n, index = 5000, count_b = 0, count_w = 0, count_g = 0;
+    int n, m, index = 5000, count_b = 0, count_w = 0, count_g = 0;
     Tile tile[10005];
     char d;
 
     cin >> n;
 
     for(int i = 0; i < n; i++){
-        cin >> n >> d;
+        cin >> m >> d;
 
         if(d == 'R'){
-            for(int i = 0; i < n; i++){
-                 if(tile[index].color != 'G'){
+            for(int i = 1; i <= m; i++){
+                
+                if(tile[index].color != 'G'){
                     tile[index].color = 'B';
-                    tile[index++].cb += 1;
-                }
-                else{
-                    index++;
-                }
-
-                if(tile[index - 1].cb >= 2 && tile[index - 1]. cw >= 2){
-                    tile[index - 1].color = 'G';
-                }
-
-            }
-        }
-        else {
-            for(int i = 0; i < n; i++){
-                if(tile[index - 1].color != 'G'){
-                    tile[index - 1].color = 'W';
-                    tile[--index].cw += 1;
-                }
-                else {
-                    --index;
+                    tile[index].cb += 1;
                 }
 
                 if(tile[index].cb >= 2 && tile[index]. cw >= 2){
                     tile[index].color = 'G';
+                }
+
+                if(i != m){
+                    index++;
+                }
+            }
+        }
+        else {
+            for(int i = 1; i <= m; i++){
+                if(tile[index].color != 'G'){
+                    tile[index].color = 'W';
+                    tile[index].cw += 1;
+                }
+
+                if(tile[index].cb >= 2 && tile[index]. cw >= 2){
+                    tile[index].color = 'G';
+                }
+
+                if(i != m){
+                    index--;
                 }
             }
         }
