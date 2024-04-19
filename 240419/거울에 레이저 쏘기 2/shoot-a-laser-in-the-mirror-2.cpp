244ba7cad8b7c60ba84bf86cacdc52196e3d817c
YJ17 <pl3434on@gmail.com>
2024-04-19 19:53:11 +0900

    Title: 거울에 레이저 쏘기 2, Time: 14ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240419/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int dx[4] = {1, 0, 0, -1}, dy[4] = {0, 1, -1, 0};
+char grid[1001][1001];
+
+int SetInit(int &x, int &y, int &d){
+    if((double)d / n <= 1){
+        x = 0;
+        y = d - 1;
+        return 3;
+    }
+    else if((double)d / n <= 2){
+        x = (d - 1) % n;
+        y = n - 1;
+        return 2;
+    }
+    else if((double)d / n <= 3){
+        x = n - 1;
+        y = (2 - (d - 1) % n);
+        return 0;
+    }
+    else {
+        x = (2 - (d - 1) % n);
+        y = 0;
+        return 1;
+    }
+}
+
+bool InRange(int x, int y){
+    return x >= 0 && x < n && y >= 0 && y < n;
+}
+
+int LazerOn(int i, int j, int dir){
+    if(dir == 3){
+        if(grid[i][j] == '/'){
+            return 2;
+
+        }
+        else {
+            return 1;
+        }
+    }
+    else if(dir == 2){
+         if(grid[i][j] == '/'){
+            return 3;
+
+        }
+        else {
+            return 0;
+        }
+ 
+    }
+    else if(dir == 0){
+         if(grid[i][j] == '/'){
+            return 1;
+
+        }
+        else {
+            return 2;
+        }
+    }
+    else {
+         if(grid[i][j] == '/'){
+            return 0;
+
+        }
+        else {
+            return 3;
+        }
+    }
+}
+
+int CountMov(int d){
+    int dir, x = 0, y = 0;
+    int count = 1;
+
+    dir = SetInit(x, y, d);
+
+    while(InRange(x, y)){
+        dir = LazerOn(x, y, dir);
+        count++;
+        x += dx[dir];
+        y += dy[dir];
+    }
+
+    return count;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int k;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> grid[i][j];
+        }
+    }
+
+    cin >> k;
+
+    cout << CountMov(k);
+
+    return 0;
+}
