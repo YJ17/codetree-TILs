9d746329fe90ad368bb64a5dd9777aaa2a1a21c3
YJ17 <pl3434on@gmail.com>
2024-04-20 20:18:31 +0900

    Title: 일렬로 서있는 소 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240420/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
+++ 240420/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
@@ -14,10 +14,10 @@ int main() {
         cin >> arr[i];
     }
 
-    for(int i = 0; i < n -2; i++){
+    for(int i = 0; i < n - 2; i++){
         for(int j = i + 1; j < n - 1; j++){
             for(int k = j + 1; k < n; k++){
-                if(arr[i] < arr[j] && arr[j] < arr[k]){
+                if(arr[i] <= arr[j] && arr[j] <= arr[k]){
                     count++;
                 }
             }
