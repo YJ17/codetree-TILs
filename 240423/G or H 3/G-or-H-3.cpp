62d288c69a5525458b9d3e15466580edc4a3bdc3
YJ17 <pl3434on@gmail.com>
2024-04-23 22:36:18 +0900

    Title: G or H 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240423/G or H 3/G-or-H-3.cpp
+++ 240423/G or H 3/G-or-H-3.cpp
@@ -7,7 +7,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
 
     int n, k, index, max_i = 0, sum = 0, max_val = -1;
-    int score[10001] = {};
+    int score[10002] = {};
     char q;
 
     cin >> n >> k;
@@ -16,14 +16,14 @@ int main() {
         cin >> index >> q;
         max_i = max(index, max_i);
         if(q == 'G'){
-            score[index - 1] = 1;
+            score[index] = 1;
         }
         else {
-            score[index - 1] = 2;
+            score[index] = 2;
         }
     }
 
-    for(int i = 0; i < max_i - k; i++){
+    for(int i = 1; i < max_i - k + 1; i++){
         for(int j = i; j <= i + k; j++){
             sum += score[j];
         }
