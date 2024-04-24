942676d58e6e2c661be66f68fb579c709febbc8e
YJ17 <pl3434on@gmail.com>
2024-04-24 19:37:59 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240424/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX_NUM 40000
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n, answer = MAX_NUM * 2;
+    int xy[101][2];
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> xy[i][0] >> xy[i][1];
+    }
+
+    for(int i = 0; i < n; i++){
+        int min_x = MAX_NUM + 1, max_x = -1;
+        int min_y = MAX_NUM + 1, max_y = -1;
+        for(int j = 0; j < n; j++){
+            if(i == j){
+                continue;
+            }
+
+            min_x = min(min_x, xy[j][0]);
+            max_x = max(max_x, xy[j][0]);
+            min_y = min(min_y, xy[j][1]);
+            max_y = max(max_y, xy[j][1]);
+        }
+        answer = min(answer, (max_x - min_x) * (max_y - min_y));
+    }
+
+    cout << answer;
+
+    return 0;
+}
