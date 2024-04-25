eb914a7cb684d2bba9000fb1b3ffa082d910b064
YJ17 <pl3434on@gmail.com>
2024-04-26 08:35:47 +0900

    Title: 상해버린 치즈, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240426/상해버린 치즈/rotten-cheese.cpp
+++ 240426/상해버린 치즈/rotten-cheese.cpp
@@ -11,12 +11,19 @@ class EatCheeseSec{
         EatCheeseSec() {}
 };
 
+class AcheSec{
+    public :
+        int p_num[51] = {};
+
+    AcheSec() {}
+};
+
 int main() {
     // 여기에 코드를 작성해주세요.
 
     int n, m, d, s, person, cheese, sec, answer = -1, index = 0;
     EatCheeseSec eat_list[101];
-    int ache_list[101] = {};
+    AcheSec ache_list[101];
     int cheese_list[51] = {};
 
     cin >> n >> m >> d >> s;
@@ -28,17 +35,23 @@ int main() {
         index++;
     }
 
+    index = 0;
     for(int i = 0; i < s; i++){
         cin >> person >> sec;
-        ache_list[sec] = person;
+        ache_list[sec].p_num[index] = person;
+        index++;
     }
 
     for(int i = 1; i < 101; i++){
-        if(ache_list[i] != 0){
-            for(int j = 1; j < i; j++){
-                for(int k = 0; k < 51; k++){
-                    if(eat_list[j].p_num[k] == ache_list[i]){
-                        cheese_list[eat_list[j].ch_num[k]] += 1;
+        for(int a = 0; a < 51; a++){
+            if(ache_list[i].p_num[a] != 0){
+                int already_eat[51] = {};
+                for(int j = 1; j < i; j++){
+                    for(int k = 0; k < 51; k++){
+                        if(eat_list[j].p_num[k] == ache_list[i].p_num[a] && already_eat[eat_list[j].ch_num[k]] == 0){
+                            cheese_list[eat_list[j].ch_num[k]] += 1;
+                            already_eat[eat_list[j].ch_num[k]] = 1;
+                        }
                     }
                 }
             }
