6b10f4578d99b658e8d03179979736c58a21312c
YJ17 <pl3434on@gmail.com>
2024-04-25 21:54:57 +0900

    Title: 삼각형 만들기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240425/삼각형 만들기/create-triangle.cpp
+++ 240425/삼각형 만들기/create-triangle.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <climits>
+#include <cstdlib>
 
 using namespace std;
 
@@ -8,6 +9,7 @@ int main() {
 
     int n, max_sp = INT_MIN;
     int arr[101][2];
+    bool state = false;
 
     cin >> n;
 
@@ -15,19 +17,28 @@ int main() {
         cin >> arr[i][0] >> arr[i][1];
     }
 
-    for(int i = 0; i < n - 1; i++){
-        for(int j = i + 1; j < n; j++){
-            if(arr[i][0] == arr[j][0]){
-                if(arr[i][1] == 0 || arr[j][1] == 0){
-                    int maxy = max(arr[i][1], arr[j][1]);
+    for(int i = 0; i < n - 2; i++){
+        for(int j = i + 1; j < n - 1; j++){
+           for(int k = j + 1; k < n; k++){
+                if(arr[i][0] == arr[j][0] || arr[j][0] == arr[k][0] || arr[k][0] == arr[i][0]){
+                    if(arr[i][1] == arr[j][1] || arr[j][1] == arr[k][1] || arr[k][1] == arr[i][1]){
+                        state = true;
+                        int cal1 = arr[i][0]*arr[j][1] + arr[j][0]*arr[k][1] + arr[k][0]*arr[i][1];
+                        int cal2 = arr[j][0]*arr[i][1] + arr[k][0]*arr[j][1] + arr[i][0] *arr[k][1];
 
-                    max_sp = max(max_sp, maxy * arr[i][0]);
+                        max_sp = max(max_sp, abs(cal1 - cal2));
+                    }
                 }
-            }
+           }
         }
     }
 
-    cout << max_sp;
+    if(state == false){
+        cout << 0;
+    }
+    else {
+        cout << max_sp;
+    }
 
     return 0;
 }
