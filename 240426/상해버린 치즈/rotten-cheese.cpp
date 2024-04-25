1a39cec6935f2ce10911df715b5f142584a61302
YJ17 <pl3434on@gmail.com>
2024-04-26 08:00:53 +0900

    Title: 상해버린 치즈, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240426/상해버린 치즈/rotten-cheese.cpp
+++ 240426/상해버린 치즈/rotten-cheese.cpp
@@ -5,30 +5,27 @@ using namespace std;
 
 class EatCheeseSec{
     public :
-        int p_num;
-        int ch_num;
+        int p_num[51] = {};
+        int ch_num[51] = {};
 
-        EatCheeseSec(int p_num = 0, int ch_num = 0){
-            this->p_num = p_num;
-            this->ch_num = ch_num;
-        }
+        EatCheeseSec() {}
 };
 
 int main() {
     // 여기에 코드를 작성해주세요.
 
-    int n, m, d, s, person, cheese, sec, answer = -1;
+    int n, m, d, s, person, cheese, sec, answer = -1, index = 0;
     EatCheeseSec eat_list[101];
     int ache_list[101] = {};
     int cheese_list[51] = {};
-    int ache_person_list[51] = {};
 
     cin >> n >> m >> d >> s;
 
     for(int i = 0; i < d; i++){
         cin >> person >> cheese >> sec;
-        eat_list[sec].p_num = person;
-        eat_list[sec].ch_num = cheese;
+        eat_list[sec].p_num[index] = person;
+        eat_list[sec].ch_num[index] = cheese;
+        index++;
     }
 
     for(int i = 0; i < s; i++){
@@ -39,24 +36,29 @@ int main() {
     for(int i = 1; i < 101; i++){
         if(ache_list[i] != 0){
             for(int j = 1; j < i; j++){
-                if(eat_list[j].p_num == ache_list[i]){
-                    cheese_list[eat_list[j].ch_num] = 1;
+                for(int k = 0; k < 51; k++){
+                    if(eat_list[j].p_num[k] == ache_list[i]){
+                        cheese_list[eat_list[j].ch_num[k]] += 1;
+                    }
                 }
             }
         }
     }
 
     for(int i = 1; i <= m; i++){
-        if(cheese_list[i] != 0){
+        if(cheese_list[i] == s){
             int count = 0;
+            int ache_person_list[51] = {};
             for(int j = 1; j < 101; j++){
-                if(eat_list[j].ch_num == i){
-                    ache_person_list[eat_list[j].p_num] = 1;
+                for(int k = 0; k < 51; k++){
+                    if(eat_list[j].ch_num[k] == i){
+                        ache_person_list[eat_list[j].p_num[k]] = 1;
+                    }
                 }
             }
 
-            for(int j = 1; j < 51; j++){
-                if(ache_person_list[j] != 0){
+            for(int j = 1; j <= n; j++){
+                if(ache_person_list[j] == 1){
                     count++;
                 }
             }
@@ -64,6 +66,8 @@ int main() {
             answer = max(answer, count);
         }
     }
+
+
 
     cout << answer;
 
