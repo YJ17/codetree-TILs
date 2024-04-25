8af0ef5e65cd03bdcdc3f3940577ea21219db66a
YJ17 <pl3434on@gmail.com>
2024-04-25 23:09:58 +0900

    Title: 겹치지 않는 선분 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240425/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
+++ 240425/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
 
-    int n, count = 0;
+    int n, answer = 0;
     int x[101][2];
+    int count[101] = {};
 
     cin >> n;
 
@@ -15,17 +16,24 @@ int main() {
     }
 
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
+        for(int j = i + 1; j < n; j++){
             if(j == i)
                 continue;
 
             if((x[i][0] >= x[j][0] && x[i][1] <= x[j][1]) || (x[j][0] >= x[i][0] && x[j][1] <= x[i][1])){
-                count++;
+                count[i] = 1;
+                count[j] = 1;
             }
         }
     }
 
-    cout << n - count;
+    for(int i = 0; i < n; i++){
+        if(count[i] == 0){
+            answer++;
+        }
+    }
+
+    cout << answer;
 
     return 0;
 }
