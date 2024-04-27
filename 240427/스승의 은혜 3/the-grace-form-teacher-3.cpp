0d56bfc854f82edf82b2605251c23d9e62a3a9b1
YJ17 <pl3434on@gmail.com>
2024-04-27 19:24:44 +0900

    Title: 스승의 은혜 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240427/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class PresentFee{
+    public :
+        int present;
+        int sendfee;
+
+        PresentFee(int present, int sendfee){
+            this->present = present;
+            this->sendfee = sendfee;
+        }
+
+        PresentFee() {}
+};
+
+bool cmp(const PresentFee &a, const PresentFee &b){
+    return a.present < b.present;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n, b, answer = -1;
+    PresentFee present_list[1001];
+
+
+    cin >> n >> b;
+
+    for(int i = 0; i < n; i++){
+        cin >> present_list[i].present >> present_list[i].sendfee;
+    }
+
+    sort(present_list, present_list + n, cmp);
+
+    for(int i = 0; i < n; i++){
+        int sum = 0;
+        int count = 0;
+        for(int j = 0; j < n; j++){
+            if(i == j){
+                sum += present_list[j].present / 2;
+            }
+            else {
+                sum += present_list[j].present;
+            }
+
+            sum += present_list[j].sendfee;
+
+            if(sum <= b){
+                count++;
+            }
+        }
+        answer = max(answer, count);
+    }
+
+    cout << answer;
+
+    return 0;
+}
