844bfc3021241578c025241f5540b4ea68567717
YJ17 <pl3434on@gmail.com>
2024-04-30 08:28:46 +0900

    Title: 등차수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240430/등차수열/arithmetic-sequence.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n, answer = 0;
+    int arr[101];
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 1; i <= 100; i++){
+        for(int j = 0; j < n - 1; j++){
+            for(int k = j + 1; k < n; k++){
+                if(arr[k] - i == i - arr[j]){
+                    answer++;
+                }
+            }
+        }
+    }
+
+    cout << answer;
+
+    return 0;
+}
