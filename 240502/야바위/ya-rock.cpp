9e839d0af3059aa9bd7e3f3f5f2581d8c24b275b
YJ17 <pl3434on@gmail.com>
2024-05-02 01:17:48 +0900

    Title: 야바위, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240502/야바위/ya-rock.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n, answer = -1;
+    int arr[101][3];
+    int num[3];
+    int suffle[101][3];
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
+    }
+
+    for(int j = 0; j < 3; j++){
+        suffle[0][j] = j + 1;
+    }
+
+    for(int j = 0; j < n; j++){      
+        if(n % 2 == 1) {
+            int temp = suffle[j][arr[j][0] - 1];
+            suffle[j][arr[j][0] - 1] = suffle[j][arr[j][1] - 1];
+            suffle[j][arr[j][1] - 1] = temp;
+        }
+        else {
+            suffle[j][0] = arr[j][0];
+            suffle[j][1] = arr[j][1];
+            suffle[j][2] = arr[j][2];
+        }
+
+        if(j + 1 < n){
+            suffle[j + 1][0] = suffle[j][0];
+            suffle[j + 1][1] = suffle[j][1];
+            suffle[j + 1][2] = suffle[j][2];
+        }
+    }
+
+    for(int i = 1; i < 4; i++){
+        int count = 0;
+        for(int j = 0; j < n; j++){
+            if(i == suffle[j][arr[j][2] - 1]){
+                count++;
+            }
+        }
+
+        answer = max(answer, count);
+    }
+
+    cout << answer;
+
+    return 0;
+}
