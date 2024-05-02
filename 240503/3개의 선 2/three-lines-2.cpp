60d37a60fe10358dc2b9210756f767949c5b6a65
YJ17 <pl3434on@gmail.com>
2024-05-03 08:38:47 +0900

    Title: 3개의 선 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240503/3개의 선 2/three-lines-2.cpp
+++ 240503/3개의 선 2/three-lines-2.cpp
@@ -15,75 +15,70 @@ int main() {
         cin >> arr[i][0] >> arr[i][1];
     }
 
-    int checkx[21] = {}, checky[21] = {};
-    int countx = 0, county = 0;
     int numx[11] = {}, numy[11] = {};
+    int countx = 0, county = 0;
+    int checkx[21] = {}, checky[21] = {};
 
     for(int i = 0; i < n - 1; i++){
         for(int j = i + 1; j < n; j++){
-            if(arr[i][0] == arr[j][0] && (checkx[i] == 0 || checkx[j] == 0)){
+            if (arr[i][0] == arr[j][0] && (checkx[i] == 0 || checkx[j] == 0)) {
                 checkx[i] = 1;
                 checkx[j] = 1;
             }
 
-            if(arr[i][0] == arr[j][0] && numx[arr[i][0]] == 0){
+            if (arr[i][0] == arr[j][0] && numx[arr[i][0]] == 0) {
                 numx[arr[i][0]] = 1;
                 countx++;
             }
-        }
-    }
-    bool nozero = true;
-    for(int i = 0; i < n; i++){
-        if(checkx[i] == 0)
-            nozero = false;
-    }
-    if(nozero == true && countx == 3){
-        state = true;
-    }
-    else {
-        for(int i = 0; i < n - 1; i++){
-            for(int j = i + 1; j < n; j++){
-                if(arr[i][1] == arr[j][1] && (checky[i] == 0 || checky[j] == 0)){
-                    checky[i] = 1;
-                    checky[j] = 1;
-                    
-                }
 
-                if(arr[i][1] == arr[j][1] && numy[arr[i][1]] == 0){
-                    numy[arr[i][1]] = 1;
-                    county++;
-                }
+            if (arr[i][1] == arr[j][1] && (checky[i] == 0 || checky[j] == 0)) {
+                checky[i] = 1;
+                checky[j] = 1;
+
+            }
+
+            if (arr[i][1] == arr[j][1] && numy[arr[i][1]] == 0) {
+                numy[arr[i][1]] = 1;
+                county++;
             }
         }
     }
-    nozero = true;
-    for(int i = 0; i < n; i++){
-        if(checky[i] == 0)
-            nozero = false;
-    }
-    if(nozero == true && county == 3){
-        state = true;
-    }
-    else {
-        int answer[21] = {};
+
+    int count_rest = 0;
+    int rest[11] = {};
+    if(countx < county){
         for(int i = 0; i < n; i++){
-            if(checkx[i] != 0)
-                answer[i] = 1;
-            if(checky[i] != 0)
-                answer[i] = 1;
+            if(checkx[i] == 0){
+                rest[arr[i][1]] = 1;
+            }
+        }
+        for(int i = 0; i < 11; i++){
+            if(rest[i] == 1)
+                count_rest++;
         }
 
-        nozero = true;
-        for(int i = 0; i < n; i++){
-            if(answer == 0)
-                nozero = false;
+        if(countx + count_rest == 3){
+            state = true;
         }
-        if((countx < 3 || county < 3) && countx + county >= 3 && nozero == true){
+    }
+    else {
+        for(int i = 0; i < n; i++){
+            if(checky[i] == 0){
+                rest[arr[i][0]] = 1;
+            }
+        }
+        for(int i = 0; i < 11; i++){
+            if(rest[i] == 1)
+                count_rest++;
+        }
+
+        if(county + count_rest == 3){
             state = true;
         }
     }
 
-    if(state){
+
+    if (state) {
         cout << 1;
     }
     else {
