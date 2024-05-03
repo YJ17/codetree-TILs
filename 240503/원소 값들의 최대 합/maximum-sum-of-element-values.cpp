1123300831354828bbbf8dd66db72e7ec03dc321
YJ17 <pl3434on@gmail.com>
2024-05-03 10:24:39 +0900

    Title: 원소 값들의 최대 합, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240503/원소 값들의 최대 합/maximum-sum-of-element-values.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n, m, answer = -1;
+    int arr[101];
+    int mov[10001];
+
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n; i++){
+        int s_index = i;
+        for(int j = 0; j < m; j++){
+            mov[j] = arr[s_index];
+            s_index = arr[s_index] - 1;
+        }
+
+        int sum = 0;
+        for(int j = 0; j < m; j++){
+            sum += mov[j];
+        }
+
+        answer = max(answer, sum);
+
+    }
+
+    cout << answer;
+
+    return 0;
+}
