bb1c9e838ba4db8a6cf75bc7ee28fc4ffe6a6f5e
YJ17 <pl3434on@gmail.com>
2024-05-03 12:19:29 +0900

    Title: 가장 많이 나온 쌍, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240503/가장 많이 나온 쌍/the-most-frequent-pair.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n, m, answer = -1;
+    int ab[101][2];
+
+    cin >> n >> m;
+
+    for(int i = 0; i < m; i++){
+        cin >> ab[i][0] >> ab[i][1];
+    }
+
+    for(int i = 1; i < n; i++){
+        for(int j = 1; j < n; j++){
+            if(i == j)
+                continue;
+            int count = 0;
+            for(int k = 0; k < m; k++){
+                if((ab[k][0] == i && ab[k][1] == j) || (ab[k][0] == j && ab[k][1] == i)){
+                    count++;
+                }
+            }
+            answer = max(answer, count);
+        }
+    }
+
+    cout << answer;
+
+    return 0;
+}
