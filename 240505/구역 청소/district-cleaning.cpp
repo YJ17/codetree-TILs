cafac53df277eb1e2b4c8f94fa961942dc1549d2
YJ17 <pl3434on@gmail.com>
2024-05-05 16:39:00 +0900

    Title: 구역 청소, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240505/구역 청소/district-cleaning.cpp
+++ 240505/구역 청소/district-cleaning.cpp
@@ -9,20 +9,11 @@ int main() {
 
     cin >> a >> b >> c >> d;
 
-    if(a <= c && a <= d && c <= b && d <= b){
-        cout << b - a;
-    }
-    else if(c <= a && c <= b && a <= d && b <= d){
-        cout << d - c;
-    }
-    else if(a <= d && d <= b){
-        cout << b - c;
-    }
-    else if(c <= b && a <= c){
-        cout << d - a;
+    if(b < c || d < a){
+        cout << b - a + d - c;
     }
     else {
-        cout << b - a + d - c;
+        cout << max(b, d) - min(a, c);
     }
 
     return 0;
